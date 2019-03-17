0722de5a70df3e61a891b5c7d3ebfef717e4f039
Oliver Albers <oliver@albers-edv.com>
2019-03-17 12:39:26 +0100

    Remove self hosted backend

--- composer.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Proxy for backend's composer command - run in container
-docker-compose exec web composer "$@"
--- docker/backend-serve.sh
+++ docker/backend-serve.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd /work
-/work/bin/console serve:run 0.0.0.0:8000
+php artisan serve --host=0.0.0.0 --port=8000
