51b849dd7c45bca096ce7063902def7890af2760
Oliver Albers <oliver@albers-edv.com>
2019-06-24 21:07:07 +0200

    Automatically install yarn packages
    On first start (i.e. when node_modules is missing) all packages
    will be automatically installed

--- docker/backend-serve.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd /work
-php artisan serve --host=0.0.0.0 --port=8000
--- docker/frontend-serve.sh
+++ docker/frontend-serve.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 cd /src
+
+# Install yarn packages if node-modules
+if [[ ! -d "node_modules" ]]; then
+  echo "Node modules doesn't exist, install packages"
+  yarn install
+fi
+
 yarn start
