c6d06f4717eae22dfd2d5c9101bb42925d808c06
Oliver Albers <oliver@albers-edv.com>
2019-06-26 00:45:01 +0200

    Add export function to save excel files

--- frontend/src/pages/ExportAllStock.js
+++ frontend/src/pages/ExportAllStock.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import ProgressSpinner from "../components/ProgressSpinner";
 import {withStitchAccess} from "../data/withStitchAccess";
 import ReactDataGrid from "react-data-grid";
+import * as Excel from "exceljs";
+import {saveAs} from 'file-saver';
 
 class ExportAllStock extends Component {
     constructor(props) {
@@ -80,15 +82,17 @@ class ExportAllStock extends Component {
 
         this.setState({
             loadingStep: 0,
-            gridColumns: this.createGridColumns(schema, Object.keys(users)),
+            gridData: this.createGridData(schema, Object.keys(users)),
             articles: articles,
             allStock: allStock,
+            users: Object.keys(users),
             indexes: this.sortGrid(articles, {}, schema[0].key, 'NONE')
         });
     }
 
-    createGridColumns(schema, users) {
+    createGridData(schema, users) {
         let gridColumns = [];
+        let xlsxColumns = [];
         let accessors = {};
 
         // Construct column definitions fpr the result grid
@@ -119,6 +123,12 @@ class ExportAllStock extends Component {
                     formatter: monospacedFont
                 });
 
+                xlsxColumns.push({
+                    key: key,
+                    name: key,
+                    width: 15
+                });
+
                 accessors[key] = (obj) => obj[key];
             }
         }
@@ -135,6 +145,12 @@ class ExportAllStock extends Component {
                     sortable: true,
                 });
 
+                xlsxColumns.push({
+                    key: key,
+                    name: key,
+                    width: 25
+                });
+
                 accessors[key] = (obj) => obj[key];
             }
         }
@@ -149,9 +165,16 @@ class ExportAllStock extends Component {
                 formatter: getCountForUser(users[i])
             });
 
+            xlsxColumns.push({
+                key: key,
+                name: users[i],
+                width: 15,
+                isAggregateColumn: true
+            });
+
             accessors[key] = (obj) => {
-                if (! obj._counts ||
-                    ! obj._counts[users[i]]) {
+                if (!obj._counts ||
+                    !obj._counts[users[i]]) {
 
                     return null;
                 }
@@ -162,6 +185,7 @@ class ExportAllStock extends Component {
 
         return {
             columns: gridColumns,
+            xlsxColumns: xlsxColumns,
             accessors: accessors
         };
     }
@@ -169,6 +193,7 @@ class ExportAllStock extends Component {
     /**
      * Return an array containing the sorted article ids of all the articles by given sort direction and column
      * @param articles
+     * @param accessors
      * @param sortColumn
      * @param sortDirection
      * @returns {string[]}
@@ -179,7 +204,7 @@ class ExportAllStock extends Component {
 
         if (sortDirection === 'NONE') {
             // No sorting, just return objects ids in order of keys
-            for(const article of articleIds) {
+            for (const article of articleIds) {
                 sortedIndexes.push(article);
             }
 
@@ -200,9 +225,149 @@ class ExportAllStock extends Component {
         return articleIds.sort(comparer);
     }
 
+    /**
+     * Export the grid with article counts as an excel file
+     * @param ev
+     * @returns {Promise<void>}
+     */
+    async exportDataToExcel(ev) {
+        ev.preventDefault();
+
+        const overviewColumns = this.state.gridData.xlsxColumns;
+        const wb = new Excel.Workbook();
+        wb.creator = process.env.REACT_APP_TITLE;
+
+        wb.created = new Date();
+        this.exportOverviewToExcel(wb, overviewColumns);
+
+
+        for(let i = 0; i < this.state.users.length; i++) {
+            const user = this.state.users[i];
+            const userStock = this.state.allStock.filter(a => a.user_email === user && a.counted && a.counted.length > 0);
+
+            if (userStock.length === 0) {
+                continue;
+            }
+
+            this.exportUserStockDetailsToExcel(user, wb, overviewColumns, userStock);
+        }
+
+        const buf = await wb.xlsx.writeBuffer();
+
+        saveAs(new Blob([buf]),
+            process.env.REACT_APP_TITLE.replace(' ', '_') + '-' +
+            wb.created.toISOString().substring(0, 10) +
+            '.xlsx');
+    }
+
+    /**
+     * Export the overview of all articles to an excel sheet
+     * @param wb
+     * @param overviewColumns
+     */
+    exportOverviewToExcel(wb, overviewColumns) {
+        const sheetOverview = wb.addWorksheet('Overview');
+        sheetOverview.autoFilter = 'A1:Z1';
+        sheetOverview.views = [
+            {state: 'frozen', xSplit: 0, ySplit: 1, topLeftCell: 'A2', activeCell: 'A2'}
+        ];
+
+        // Write headers
+        let headerData = [];
+        for (let i = 0; i < overviewColumns.length; i++) {
+            headerData.push(overviewColumns[i].name);
+        }
+        const row = sheetOverview.addRow(headerData);
+        row.font = {bold: true};
+        row.commit();
+
+        // Write data rows of sorted articles
+        const accessors = this.state.gridData.accessors;
+        for (let row = 0; row < this.state.indexes.length; row++) {
+            const article = this.state.articles[this.state.indexes[row]];
+            let rowData = [];
+
+            for (let column = 0; column < overviewColumns.length; column++) {
+                const data = accessors[overviewColumns[column].key](article);
+                rowData.push(data);
+            }
+
+            const datarow = sheetOverview.addRow(rowData);
+            datarow.font = {bold: false};
+            datarow.commit();
+        }
+
+        // Set column width
+        for (let column = 1; column <= overviewColumns.length; column++) {
+            let col = sheetOverview.getColumn(column);
+            col.width = overviewColumns[column - 1].width;
+        }
+    }
+
+    /**
+     * Export detailed information about every change in stock count for a single user
+     * @param user
+     * @param wb
+     * @param overviewColumns
+     * @param userStock
+     */
+    exportUserStockDetailsToExcel(user, wb, overviewColumns, userStock) {
+        let sheetName = user;
+        if (sheetName.length > 30) {
+            sheetName = sheetName.substr(0, 30);
+        }
+        const sheetUser = wb.addWorksheet(sheetName);
+        sheetUser.autoFilter = 'A1:Z1';
+        sheetUser.views = [
+            {state: 'frozen', xSplit: 0, ySplit: 1, topLeftCell: 'A2', activeCell: 'A2'}
+        ];
+
+        const accessors = this.state.gridData.accessors;
+
+        let headerData = [];
+        let detailAccessors = [];
+
+        for (let i = 0; i < overviewColumns.length; i++) {
+            if (overviewColumns[i].isAggregateColumn) {
+                continue;
+            }
+
+            headerData.push(overviewColumns[i].name);
+            detailAccessors.push(accessors[overviewColumns[i].key]);
+        }
+        headerData.push('Count Date');
+        detailAccessors.push((obj, i) => {
+            return obj.counted[i].created_at;
+        });
+        headerData.push('Count');
+        detailAccessors.push((obj, i) => {
+            return obj.counted[i].change;
+        });
+
+        const row = sheetUser.addRow(headerData);
+        row.font = {bold: true};
+        row.commit();
+
+        for (let stockIndex = 0; stockIndex < userStock.length; stockIndex++) {
+            const stock = userStock[stockIndex];
+
+            for (let changeRow = 0; changeRow < stock.counted.length; changeRow++) {
+                let rowData = [];
+
+                for (let column = 0; column < detailAccessors.length; column++) {
+                    rowData.push(detailAccessors[column](stock, changeRow));
+                }
+
+                const datarow = sheetUser.addRow(rowData);
+                datarow.font = {bold: false};
+                datarow.commit();
+            }
+        }
+    }
+
     showStockDataGrid() {
         if (this.state.loadingStep > 0 ||
-            !this.state.gridColumns ||
+            !this.state.gridData ||
             !this.state.articles) {
 
             return null;
@@ -210,14 +375,16 @@ class ExportAllStock extends Component {
 
         return <>
             <ReactDataGrid
-                columns={this.state.gridColumns.columns}
+                columns={this.state.gridData.columns}
                 rowGetter={i => this.state.articles[this.state.indexes[i]]}
                 rowsCount={this.state.indexes.length}
                 onGridSort={(sortColumn, sortDirection) => {
-                    const sorted = this.sortGrid(this.state.articles, this.state.gridColumns.accessors, sortColumn, sortDirection);
+                    const sorted = this.sortGrid(this.state.articles, this.state.gridData.accessors, sortColumn, sortDirection);
                     this.setState({indexes: sorted});
                 }}
             />
+
+            <button onClick={this.exportDataToExcel.bind(this)}>Download Excel</button>
         </>;
     }
 
